95a50a7916f8fb9aa8d17ea3c1305ae26ab324f2
GerritHillebrecht <gerrit.hillebrecht@gmail.com>
2025-08-08 17:57:52 +0200

    feat(config): add image remote patterns for Sanity CDN

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+        port: "",
+        pathname: "/images/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
-
-
